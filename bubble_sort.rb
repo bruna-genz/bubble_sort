a8f87ebb7bb36ff1b893a244752142a38c97f364
Bruna <brunagenz92@gmail.com>
2019-09-11 17:20:30 -0300

    fixed linter errors again

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 def bubble_sort(arr)
   loop do
     replace = false
@@ -17,7 +18,7 @@ def bubble_sort_by(arr)
   loop do
     replace = false
     for i in 0...arr.length - 1
-    comparison = yield(arr[i], arr[i + 1])
+      comparison = yield(arr[i], arr[i + 1])
       if comparison.positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         replace = true
