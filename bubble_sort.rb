ca56316ab3b0d42d6ac9c5aabd5b0be47dab61c1
Bruna <brunagenz92@gmail.com>
2019-09-11 16:56:39 -0300

    fixed linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,29 +1,28 @@
-def bubble_sort(arr) #[2,3,4,2,4.5]
-    loop {
-        replace = false
-        for i in 0...arr.length-1
-            if arr[i] > arr[i+1]
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                replace = true
-            end
-        end
-        break if !replace
-    }
-    arr
+def bubble_sort(arr)
+  loop {
+    replace = false
+    for i in 0...arr.length - 1
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        replace = true
+      end
+    end
+    break unless !replace
+  }
+  arr
 end
 
-bubble_sort([4,3,78,2,0,2])
-
 def bubble_sort_by(arr)
-    loop {
-        replace = false
-        for i in 0...arr.length-1
-            comparison = yield(arr[i], arr[i+1])
-            if comparison > 0
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                replace = true
-            end
+  loop {
+      replace = false
+      for i in 0...arr.length - 1
+        comparison = yield(arr[i], arr[i + 1])
+        if comparison.positive?
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+          replace = true
         end
-        break if !replace
-    }
+      end
+    break unless !replace
+  }
+  arr
 end
