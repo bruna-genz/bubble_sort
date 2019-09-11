8b62b60b716ae3190b620c7b6f654e5fa88a37d1
Bruna <brunagenz92@gmail.com>
2019-09-11 16:41:14 -0300

    bubble-sort-by method created

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,3 +13,17 @@ def bubble_sort(arr) #[2,3,4,2,4.5]
 end
 
 bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(arr)
+    loop {
+        replace = false
+        for i in 0...arr.length-1
+            comparison = yield(arr[i], arr[i+1])
+            if comparison > 0
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                replace = true
+            end
+        end
+        break if !replace
+    }
+end
